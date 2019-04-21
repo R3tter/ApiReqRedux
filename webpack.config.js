bf8c8ad5fa6c3c2248dc29461a7b874081c0c480
Yaroslav <uwebtechnology@gmail.com>
2019-04-21 18:46:39 +0300

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
-    library: 'api-request-redux',
+    library: 'api-req-redux',
     libraryTarget: 'umd'
   }
 };
